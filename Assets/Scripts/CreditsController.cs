13e0238f2f3c5bf243eb94a6fe5012ef2f6c2ff3
whc823 <wanghuac@uci.edu>
2025-04-06 23:19:55 -0700

    Audio Manager V0.8

--- Assets/Scripts/CreditsController.cs
+++ Assets/Scripts/CreditsController.cs
@@ -5,11 +5,36 @@ using UnityEngine.SceneManagement;
 public class CreditsController : MonoBehaviour
 {
     [SerializeField] private string mainMenuSceneName = "MainMenu";
+   
+    // Sound
+    [SerializeField] string hoverOverSound = "ButtonHover";
+    [SerializeField] string clickButtonSound = "ButtonClick";
 
+    AudioManager audioManager;
+
+    void Start()
+    {
+        audioManager = AudioManager.instance;
+        if (audioManager == null)
+        {
+            Debug.LogError("No audio manager found");
+        }
+    }
+
+    // Play sound when mouse hovering UI 
+    public void OnMouseOver()
+    {
+        audioManager.PlaySound(hoverOverSound);
+    }
     // This method can be directly called from the button's OnClick event
     public void BackToMainMenu()
     {
         Debug.Log("Returning to main menu from credits");
+        
+        // Play Click sound
+        audioManager.PlaySound(clickButtonSound);
+
         SceneManager.LoadScene(mainMenuSceneName);
     }
+
 }
